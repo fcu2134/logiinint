cdbd687ca2decb5881aa042e0e727f96607e684a
fcu2134 <facundo.marin.moscol@cftmail.cl>
2024-05-29 21:43:32 -0400

    login

---
+++ app/src/main/java/com/example/logiinint7/Login.java
@@ -0,0 +1,6 @@
+package com.example.logiinint7;
+
+import android.app.Activity;
+
+public class Login extends Activity {
+}
---
+++ app/src/main/java/com/example/logiinint7/registratation.kt
@@ -0,0 +1,4 @@
+package com.example.logiinint7
+
+class registratation {
+}
